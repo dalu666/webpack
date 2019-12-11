26571df1a04fa16feb0f44616641f5e3fb3244b4
lugit <luliuju619@163.com>
2019-12-11 17:22:22 +0800

    提交文件

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import _ from "lodash";
+import './style.css'
+import Icon from './myjpg.jpg'
+
+function component(){
+    let element=document.createElement('div');
+    // Loadsh 通过script脚本引入 执行这一行是必须的。
+    element.innerHTML=_.join(['hello','webpack'],' ');
+    element.classList.add('hello');
+    // 将图片添加到我们现有的div
+    let myIcon=new Image();
+    myIcon.src=Icon;
+    element.appendChild(myIcon);
+    return element;
+}
+document.body.appendChild(component());
